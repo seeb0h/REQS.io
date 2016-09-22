a78d9fa4b5ff7bdf829a61707cfcf19c846098b6
Merry31 <julien.rallo@gmail.com>
2016-09-22 14:10:09 +0200

    modif test specification model

--- modules/specifications/tests/server/specification.server.model.tests.js
+++ modules/specifications/tests/server/specification.server.model.tests.js
@@ -6,19 +6,21 @@
 var should = require('should'),
   mongoose = require('mongoose'),
   User = mongoose.model('User'),
+  Project = mongoose.model('Project'),
   Specification = mongoose.model('Specification');
 
 /**
  * Globals
  */
 var user,
+  project,
   specification;
 
 /**
  * Unit tests
  */
-describe('Specification Model Unit Tests:', function() {
-  beforeEach(function(done) {
+describe('Specification Model Unit Tests:', function () {
+  beforeEach(function (done) {
     user = new User({
       firstName: 'Full',
       lastName: 'Name',
@@ -28,39 +30,50 @@ describe('Specification Model Unit Tests:', function() {
       password: 'password'
     });
 
-    user.save(function() {
-      specification = new Specification({
-        name: 'Specification Name',
+    user.save(function () {
+      project = new Project({
+        name: 'Project test 1',
         user: user
       });
-
-      done();
+      project.save(function () {
+        specification = new Specification({
+          name: 'Specification Name',
+          docName: 'Doc Name',
+          type: 'type',
+          version: '1.0',
+          project: project,
+          user: user
+        });
+        done();
+      });
     });
   });
 
-  describe('Method Save', function() {
-    it('should be able to save without problems', function(done) {
+  describe('Method Save', function () {
+    it('should be able to save without problems', function (done) {
       this.timeout(0);
-      return specification.save(function(err) {
+      return specification.save(function (err) {
         should.not.exist(err);
         done();
       });
     });
 
-    it('should be able to show an error when try to save without name', function(done) {
+    it('should be able to show an error when try to save without name', function (done) {
       specification.name = '';
 
-      return specification.save(function(err) {
+      return specification.save(function (err) {
         should.exist(err);
         done();
       });
     });
   });
 
-  afterEach(function(done) {
-    Specification.remove().exec(function() {
-      User.remove().exec(function() {
-        done();
+  afterEach(function (done) {
+    Specification.remove().exec(function () {
+      Project.remove().exec(function () {
+        User.remove().exec(function () {
+          done();
+        });
       });
     });
   });
